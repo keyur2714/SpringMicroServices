9f81c8febf5eb799ac8c636f6984fe3cbe2e0e9c
keyur2714 <keyur.555kn@gmail.com>
2020-08-02 11:48:15 +0530

    Added Spring Microservice Projects

---
+++ customers-micro/src/main/java/com/webstack/customers/CustomersMicroApplication.java
@@ -0,0 +1,15 @@
+package com.webstack.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class CustomersMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersMicroApplication.class, args);
+	}
+
+}
---
+++ customers-micro/src/test/java/com/webstack/customers/CustomersMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webstack.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
